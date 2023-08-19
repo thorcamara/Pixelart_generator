16727ccf5bc02f80fc925884c874dc9f3e5b02a9
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-18 23:55:04 -0300

    Defined const with arrow function and added gridButton iteraction

--- script.js
+++ script.js
@@ -26,3 +26,59 @@ let deviceType = "";
 
 let draw = false;
 let erase = false;
+
+const isTouchDevice = () => {
+  try {
+      document.createEvent("TouchEvent");
+      deviceType = "touch";
+      return true;
+  } catch (e) {
+      deviceType = "mouse";
+      return false;
+  }
+};
+
+isTouchDevice();
+
+gridButton.addEventListener("click", () => {
+  container.innerHTML = "";
+  let count = 0;
+  for (let i = 0; i < gridHeight.value; i++) {
+      count += 2;
+      let div = document.createElement("div");
+      div.classList.add("gridRow");
+
+      for (let j = 0; j < gridWidth.value; j++) {
+          count += 2;
+          let col = document.createElement("div");
+          col.classList.add("gridCol");
+          col.setAttribute("id", `gridCol${count}`);
+          col.addEventListener(events[deviceType].down, () => {
+              draw = true;
+              if (erase) {
+                  col.style.backgroundColor = "transparent";
+              } else {
+                  col.style.backgroundColor = colorButton.value;
+              }
+          });
+
+          col.addEventListener(events[deviceType].move, (e) => {
+              let elementId = document.elementFromPoint(
+                  !isTouchDevice() ? e.clientX : e.touches[0].clientX,
+                  !isTouchDevice() ? e.clientY : e.touches[0].clientY,
+              ).id;
+              checker(elementId);
+          });
+
+          col.addEventListener(events[deviceType].up, () => {
+              draw = false;
+          });
+
+          div.appendChild(col);
+
+      }
+
+      container.appendChild(div);
+
+  }
+});
